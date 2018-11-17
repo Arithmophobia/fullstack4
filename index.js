34e547d27d6707d6477f60e47668ec28c5ac687e
Arithmophobia <sannan.kanny@gmail.com>
2018-11-17 10:33:52 +0200

    Add task 4.2

--- index.js
+++ index.js
@@ -5,19 +5,15 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 
-if ( process.env.NODE_ENV !== 'production' ) {
-  require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
 }
+const url = process.env.MONGODB_URI
 
-const Blog = mongoose.model('Blog', {
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-module.exports = Blog
+mongoose.connect(url, { useNewUrlParser: true })
 
 morgan.token('data', function getData(req) {
   return req.data
@@ -35,27 +31,8 @@ app.use(assignData)
 // finally log data
 app.use(morgan(':method :url :data :status :res[content-length] - :response-time ms'))
 //const mongoUrl = 'mongodb://localhost/bloglist'
-const mongoUrl = process.env.MONGODB_URI
-
-mongoose.connect(mongoUrl, { useNewUrlParser: true })
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
+app.use(middleware.error)
 
 const PORT = 3003
 app.listen(PORT, () => {
